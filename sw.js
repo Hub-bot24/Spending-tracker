ead19374b4c638c8e2a64a2eab61577e090f6d64
Hub-bot24 <jdhugoaus@gmail.com>
2025-10-08 14:09:22 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const VERSION = 'v28';                 // bump this
+const VERSION = 'v28';               // bump this
 const ROOT = '/Spending-tracker/';
-
 const ASSETS = [
   ROOT,
   ROOT + 'index.html',
   ROOT + 'manifest.webmanifest',
   ROOT + 'food/',
   ROOT + 'food/index.html',
-  ROOT + 'savings/',            // NEW
-  ROOT + 'savings/index.html'   // NEW
+  ROOT + 'savings/',            // keep these two
+  ROOT + 'savings/index.html'
 ];
